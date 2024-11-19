3da42273711aa5f737cd8cc058bdbb2b36a75162
Vamsichris04 <115572474+Vamsichris04@users.noreply.github.com>
2024-11-18 20:39:24 -0600

    Update Alien.h
    New Alien.h

--- Alien.h
+++ Alien.h
@@ -1,23 +1,22 @@
-//
-// Created by barbulescuv on 10/28/2024.
-//
+// Authors: Victor Barbulescu & Vamsi Sudersanam
+// Course : CSC 2210/001
+// Date: 11/17/2024
 
 #ifndef ALIEN_H
 #define ALIEN_H
+
 #include "Entity.h"
 
-class Room;
-
 class Alien : public Entity{
-    bool hit = false; // If the alien has been hit or not
-    bool grabbedPlayer = false; // If the player has been grabbed by the alien or not
+private:
+    bool hit = false;
 public:
     Alien();
-    void interact(Person* p) override;
-    void printSelf() override;
-    bool isHit() const;                 // Check if the Alien has been hit
-    Room* getRoom();
-    void hitByWeapon(int range, Person* p);  // Alien is shot if within weapon range
+    ~Alien() override;
+
+    void interact(Person *p) override;
+    void hint() override;
+    char character() override;
 };
 
 
